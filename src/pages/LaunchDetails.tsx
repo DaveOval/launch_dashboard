cbe153355bce5057304d1c524e262e10caa89809
David Vazquez <dave_u@outlook.com>
2024-09-22 15:31:19 -0600

    detalles de la mision

--- src/pages/LaunchDetails.tsx
+++ src/pages/LaunchDetails.tsx
@@ -5,8 +5,9 @@ import useFavorite from "../hooks/useFavorites";
 
 interface Launch {
   flight_number: number;
-  mission_name: string;
-  launch_date_utc: string;
+  name: string;
+  date_utc: string;
+  launch_year: number | string;
   rocket: {
     rocket_name: string;
   };
@@ -17,6 +18,10 @@ interface Launch {
   links: {
     video_link: string;
     wikipedia: string;
+    patch: {
+      large: string | null;
+      small: string | null
+    };
   };
 }
 
@@ -33,48 +38,55 @@ export const LaunchDetails = () => {
     return <div>Error: {error}</div>;
   }
 
+  console.log({data})
+
   const siteName = data.launch_site?.site_name || "Lugar no encontrado";
-
-  console.log(import.meta.env.VITE_GOOGLE_MAPS_API_KEY)
-
   const mapSrc = `https://www.google.com/maps/embed/v1/place?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&q=${encodeURIComponent(siteName)}`;
 
   return (
-    <div className="p-8 bg-gray-100 min-h-screen">
-      <div className="bg-white shadow-md rounded-lg p-6">
-        <div className="flex justify-between items-center">
-          <h1 className="text-3xl font-bold mb-4">{data.mission_name}</h1>
-          <button onClick={toggleFavorite} className={`${liked ? "text-red-500" : "text-gray-500"} transition duration-300 focus:outline-none`}>
-            {liked ? "❤️ Saved to favorites" : "🤍 Add to favorites"}
-          </button>
+    <div className="p-6 md:p-8 bg-gray-100 min-h-screen flex flex-col">
+      <div className="flex flex-col md:flex-row w-full space-y-6 md:space-y-0 md:space-x-8">
+        <div className="flex-1">
+          <div className="flex justify-between items-center">
+            <h1 className="text-2xl md:text-3xl font-bold">{data.name}</h1>
+            <button onClick={toggleFavorite} className={`${liked ? "text-red-500" : "text-gray-500"} transition duration-300 focus:outline-none`}>
+              {liked ? "❤️ Saved to favorites" : "🤍 Add to favorites"}
+            </button>
+          </div>
+          <p className="text-gray-700">
+            <strong>Launch Date:</strong> {new Date(data.date_utc).toLocaleString()}
+          </p>
+          <p className="text-gray-700">
+            <strong>Launch Year:</strong> {data.launch_year || "No launch date"}
+          </p>
+          <p className="text-gray-700">
+            <strong>Rocket:</strong> {data.rocket.rocket_name || "No rocket"}
+          </p>
+          <div className="mb-4">
+            <strong>Watch the Launch:</strong>
+            <a href={data.links.video_link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Watch on YouTube</a>
+          </div>
+          <div className="mb-4">
+            <strong>More Info:</strong>
+            <a href={data.links.wikipedia} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Read on Wikipedia</a>
+          </div>
         </div>
-        <p className="text-gray-700 mb-4">
-          <strong>Launch Date:</strong> {new Date(data.launch_date_utc).toLocaleString()}
-        </p>
-        <p className="text-gray-700 mb-4">
-          <strong>Rocket:</strong> {data.rocket.rocket_name}
-        </p>
-        <div className="mb-4">
-          <strong>Watch the Launch:</strong>
-          <a href={data.links.video_link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Watch on YouTube</a>
+  
+        <div className="bg-white shadow-md rounded-lg p-4 flex-1 flex justify-center items-center"> {/* Añadir flex aquí */}
+          <img className="h-auto max-w-full rounded-md" src={`${data.links.patch.small}`} alt="image description" />
         </div>
-        <div className="mb-4">
-          <strong>More Info:</strong>
-          <a href={data.links.wikipedia} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Read on Wikipedia</a>
-        </div>
-        
-        {/* Google Maps embed using site name */}
-        <div className="h-64 w-full bg-gray-200 flex justify-center items-center">
-          <iframe
-            width="450"
-            height="250"
-            frameBorder="0"
-            referrerPolicy="no-referrer-when-downgrade"
-            src={mapSrc}
-            allowFullScreen>
-          </iframe>
-        </div>
+      </div>
+  
+      <div className="h-64 w-full mt-8 bg-gray-200 flex justify-center items-center">
+        <iframe
+          className="w-full h-full max-w-xl"
+          frameBorder="0"
+          referrerPolicy="no-referrer-when-downgrade"
+          src={mapSrc}
+          allowFullScreen>
+        </iframe>
       </div>
     </div>
   );
+  
 };
